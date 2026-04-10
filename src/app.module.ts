cc94f2aaf9be298b404689c17e777fd6cf6bdd78
anvuzmaker <aan.vuhoang@gmail.com>
2026-04-10 22:32:30 +0700

    feat: setup backend modular structure

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { EventsModule } from './modules/events/events.module';
+import { RegistrationsModule } from './modules/registrations/registrations.module';
+import { CheckinModule } from './modules/checkin/checkin.module';
+import { GamificationModule } from './modules/gamification/gamification.module';
+import { LeaderboardModule } from './modules/leaderboard/leaderboard.module';
+import { MapModule } from './modules/map/map.module';
+import { ForumModule } from './modules/forum/forum.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, EventsModule, RegistrationsModule, CheckinModule, GamificationModule, LeaderboardModule, MapModule, ForumModule, NotificationsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
