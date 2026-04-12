e8cf0595bf975c92cd25b155ef46c9488768f71e
anvuzmaker <aan.vuhoang@gmail.com>
2026-04-12 15:21:24 +0700

    feat(auth): implement core auth logic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { RegisterDto } from "./dto/register.dto";
+import { LoginDto } from "./dto/login.dto";
 
-@Controller('auth')
-export class AuthController {}
+@Controller("auth")
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  // REGISTER
+  @Post("register")
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto.email, dto.fullName, dto.password);
+  }
+
+  // LOGIN
+  @Post("login")
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto.email, dto.password);
+  }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
+import * as bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) {}
+
+  // ========================
+  // REGISTER
+  // ========================
+  async register(email: string, fullName: string, password: string) {
+    // kiểm tra user tồm tại
+    const userExist = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userExist) throw new Error("Email already exists");
+
+    // Băm passworld
+    const hash = await bcrypt.hash(password, 10);
+
+    // Tạo user
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        fullName,
+        password: hash,
+      },
+    });
+
+    return this.generateToken(user.id, user.email, user.role);
+  }
+
+  // ========================
+  // LOGIN
+  // ========================
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) throw new UnauthorizedException("Invalid credentials");
+
+    const isMatch = await bcrypt.compare(password, user.password || "");
+
+    if (!isMatch) throw new UnauthorizedException("Invalid credentials");
+
+    return this.generateToken(user.id, user.email, user.role);
+  }
+
+  // ========================
+  // JWT GENERATOR
+  // ========================
+  async generateToken(userId: string, email: string, role: string) {
+    const payload = { sub: userId, email, role };
+
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: "15m",
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: "7d",
+    });
+
+    // Lưu refresh token
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { refreshToken },
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  // ========================
+  // REFRESH TOKEN
+  // ========================
+  async refresh(userId: string, token: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user || user.refreshToken !== token) {
+      throw new UnauthorizedException("Invalid refresh token");
+    }
+
+    return this.generateToken(user.id, user.email, user.role);
+  }
+}
